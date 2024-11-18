332714ed077e3eb6a94f2082ecf1213cd84b04e4
Maksim <knyazev.maksim2852@gmail.com>
2024-11-18 12:47:29 +0400

    add new keyboard

--- src/keyboards/user_keyboards/keyboards.py
+++ src/keyboards/user_keyboards/keyboards.py
@@ -8,7 +8,20 @@ def get_keyboard() -> ReplyKeyboardMarkup:
     keyboard: ReplyKeyboardMarkup = ReplyKeyboardMarkup(
         keyboard=[
             [KeyboardButton(text='Взять ещё 🃏')],
-            [KeyboardButton(text='Хватит 🫸')]
+            [KeyboardButton(text='Хватит 🫸')],
+            [KeyboardButton(text='Отменить игру ⛔️')]
+        ],
+        resize_keyboard=True
+    )
+    return keyboard
+
+
+def game_mode_with_player() -> ReplyKeyboardMarkup:
+    keyboard: ReplyKeyboardMarkup = ReplyKeyboardMarkup(
+        keyboard=[
+            [KeyboardButton(text='Играть со ставкой 💸')],
+            [KeyboardButton(text='Играть без ставки 🤝')],
+            [KeyboardButton(text='Главное меню 💡')]
         ],
         resize_keyboard=True
     )
@@ -20,7 +33,20 @@ def get_kb_main_menu() -> ReplyKeyboardMarkup:
         keyboard=[
             [KeyboardButton(text='Играть ▶️')],
             [KeyboardButton(text='Мой профиль 📊')],
+            [KeyboardButton(text='Таблица лидеров 🏆')],
             [KeyboardButton(text='Правила ❓')]
+        ],
+        resize_keyboard=True
+    )
+    return keyboard
+
+
+def get_mode_game() -> ReplyKeyboardMarkup:
+    keyboard: ReplyKeyboardMarkup = ReplyKeyboardMarkup(
+        keyboard=[
+            [KeyboardButton(text='Играть с ботом 🤖')],
+            [KeyboardButton(text='Играть с игроками 🧑‍💻')],
+            [KeyboardButton(text='Главное меню 💡')]
         ],
         resize_keyboard=True
     )
