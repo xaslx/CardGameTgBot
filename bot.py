106f5f9ba64b53736472cef4c695639677a7a72e
Maksim <knyazev.maksim2852@gmail.com>
2024-11-18 12:47:48 +0400

    include middleware

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import logging
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from config import env_config
-from src.middlewares.db_session import DbMiddleware
+from src.middlewares.middlewares import DbMiddleware, UserCheckMiddleware
 from database import async_session_maker
 from src.handlers.user_handler.handler import router as user_router
 
@@ -33,6 +33,7 @@ async def main():
     bot: Bot = Bot(token=env_config.TOKEN_BOT, default=DefaultBotProperties(parse_mode='HTML'))
     dp: Dispatcher = Dispatcher()
     dp.update.middleware.register(DbMiddleware(async_session_maker))
+    dp.update.middleware.register(UserCheckMiddleware(async_session_maker))
     dp.include_router(user_router)
     dp.startup.register(on_startup)
     dp.shutdown.register(on_shutdown)
